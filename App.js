9ff1c4c534e67c0778d1127c1f9020e72937b8b4
chuluq <49191833+chuluq@users.noreply.github.com>
2021-07-07 07:36:35 +0700

    check internet connection

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Provider } from 'react-redux';
+import NetInfo from '@react-native-community/netinfo';
 import store from './redux/store';
 import Home from './screens/home';
+import Error from './components/Error';
+import Loading from './components/Loading';
 
 export default function App() {
+  const [connected, setConnected] = useState(null);
+
+  useEffect(() => {
+    NetInfo.fetch().then((state) => {
+      setConnected(state.isConnected);
+    });
+  });
+
   return (
     <Provider store={store}>
-      <Home />
+      {connected === null ? (
+        <Loading />
+      ) : connected === true ? (
+        <Home />
+      ) : (
+        <Error errorMessage='Not connected to Internet' />
+      )}
     </Provider>
   );
 }
